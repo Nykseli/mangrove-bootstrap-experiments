888ca645853e949bc1cc5697844bc17738798073
Miika Alikirri <nykseli@protonmail.com>
2023-09-07 18:40:33 +0300

    cli: Initial parsing and compiling file given as an arg

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::env::args;
+
+use mangrove_rs::compiler::Compiler;
+use mangrove_rs::parser::{parse::Parser, tokeniser::Tokeniser};
+
 fn main() {
-	println!("Hello, world!");
+	let args: Vec<String> = args().collect();
+	let tokeniser = Tokeniser::new(std::fs::read_to_string(&args[1]).unwrap());
+	let mut parser = Parser::new(tokeniser);
+	let ast = parser.parse();
+	let mut compiler = Compiler::new(ast);
+	println!("{}", compiler.compile());
 }
