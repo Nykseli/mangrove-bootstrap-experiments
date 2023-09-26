4b0da3f5d1eac201f9bca7ca0d83afcb287da93f
Miika Alikirri <nykseli@protonmail.com>
2023-09-26 16:56:29 +0300

    main: Optionally optimise AST

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use clap::Parser as _;
 mod cli;
 
 use mangrove_rs::compiler::Compiler;
+use mangrove_rs::optimiser::Optimiser;
 use mangrove_rs::parser::{parse::Parser, tokeniser::Tokeniser};
 
 fn main() {
@@ -10,6 +11,11 @@ fn main() {
 	let tokeniser = Tokeniser::new(std::fs::read_to_string(args.file()).unwrap());
 	let mut parser = Parser::new(tokeniser);
 	parser.parse();
+	if args.optimise() {
+		let mut optimiser = Optimiser::new(parser);
+		optimiser.optimise();
+		parser = optimiser.parser;
+	}
 	let mut compiler = Compiler::new(parser);
 	println!("{}", compiler.compile());
 }
