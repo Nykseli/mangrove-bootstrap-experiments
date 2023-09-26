9619982be0b28da30c14678b01a02eadc26a58d5
Miika Alikirri <nykseli@protonmail.com>
2023-09-26 16:58:31 +0300

    tests: Test optimised code with the test runner

--- tests/compiler_tests.sh
+++ tests/compiler_tests.sh
@@ -27,25 +27,10 @@ if [ -z "$WASM_PATH" ]; then
 	exit 1
 fi
 
-test_file() {
-	FILE=$1
-	WAT_PATH=$(echo $FILE | sed 's/\.grove/\.wat/')
-	echo Compiling $FILE
+check_output() {
+	RUN_OUT="$1"
+	OUTPUT_LINES="$2"
 
-	FIRST_LINE=$(awk 'NR==1{print $0}' $FILE)
-	if [[ "$FIRST_LINE" != "// Output:" ]]; then
-		echo First line is not starting with output, skipping.
-		continue
-	fi
-
-	# Capture lines after output until a empty line occurs
-	OUTPUT_LINES=$(awk 'NR==2,/^$/' $FILE | sed 's/\/\/ //')
-
-	printf "testing output... "
-	./target/release/mangrove-rs $FILE > $WAT_PATH
-	RUN_OUT=$(wasmtime --preload internals=asm/wasm/internals.wat \
-			 --preload main=$WAT_PATH \
-			 asm/wasm/run.wat)
 	if [[ "$RUN_OUT" == "$OUTPUT_LINES" ]]; then
 		printf "${GREEN}Success${NC}\n"
 	else
@@ -56,6 +41,36 @@ test_file() {
 		echo "$RUN_OUT"
 		exit 1
 	fi
+}
+
+test_file() {
+	FILE=$1
+	WAT_PATH=$(echo $FILE | sed 's/\.grove/\.wat/')
+	WAT_OPATH=$(echo $FILE | sed 's/\.grove/_opt\.wat/')
+	echo Compiling $FILE
+
+	FIRST_LINE=$(awk 'NR==1{print $0}' $FILE)
+	if [[ "$FIRST_LINE" != "// Output:" ]]; then
+		echo First line is not starting with output, skipping.
+		return
+	fi
+
+	# Capture lines after output until a empty line occurs
+	OUTPUT_LINES=$(awk 'NR==2,/^$/' $FILE | sed 's/\/\/ //')
+
+	printf "testing output... "
+	./target/release/mangrove-rs $FILE > $WAT_PATH
+	./target/release/mangrove-rs --optimise $FILE > $WAT_OPATH
+	RUN_OUT=$(wasmtime --preload internals=asm/wasm/internals.wat \
+			 --preload main=$WAT_PATH \
+			 asm/wasm/run.wat)
+	check_output "$RUN_OUT" "$OUTPUT_LINES"
+
+	printf "testing optimised output... "
+	RUN_OUT=$(wasmtime --preload internals=asm/wasm/internals.wat \
+			 --preload main=$WAT_OPATH \
+			 asm/wasm/run.wat)
+	check_output "$RUN_OUT" "$OUTPUT_LINES"
 }
 
 if [[ ! -z "$1" ]]; then
