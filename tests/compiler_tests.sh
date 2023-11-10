9ffb2332e1d38e7d10673cfea58d3e7a40a89ef5
Miika Alikirri <nykseli@protonmail.com>
2023-11-11 00:41:28 +0200

    tests: disable optimised tests

--- tests/compiler_tests.sh
+++ tests/compiler_tests.sh
@@ -60,17 +60,17 @@ test_file() {
 
 	printf "testing output... "
 	./target/release/mangrove-rs $FILE > $WAT_PATH
-	./target/release/mangrove-rs --optimise $FILE > $WAT_OPATH
+	# ./target/release/mangrove-rs --optimise $FILE > $WAT_OPATH
 	RUN_OUT=$(wasmtime --preload internals=asm/wasm/internals.wat \
 			 --preload main=$WAT_PATH \
 			 asm/wasm/run.wat)
 	check_output "$RUN_OUT" "$OUTPUT_LINES"
 
-	printf "testing optimised output... "
-	RUN_OUT=$(wasmtime --preload internals=asm/wasm/internals.wat \
-			 --preload main=$WAT_OPATH \
-			 asm/wasm/run.wat)
-	check_output "$RUN_OUT" "$OUTPUT_LINES"
+	# printf "testing optimised output... "
+	# RUN_OUT=$(wasmtime --preload internals=asm/wasm/internals.wat \
+	# 		 --preload main=$WAT_OPATH \
+	# 		 asm/wasm/run.wat)
+	# check_output "$RUN_OUT" "$OUTPUT_LINES"
 }
 
 if [[ ! -z "$1" ]]; then
