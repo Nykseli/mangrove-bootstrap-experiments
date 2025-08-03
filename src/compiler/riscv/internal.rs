5689ad22d9ec8c9a7f11312c07ee8dc22809b435
Miika Alikirri <duck@duckie.fi>
2025-08-03 20:15:40 +0300

    compier/riscv: compile internal __print_format function

--- src/compiler/riscv/internal.rs
+++ src/compiler/riscv/internal.rs
@@ -10,7 +10,115 @@ const INTERNAL_FUNCTIONS: &'static [(
 	("__print_int", compile_print_int),
 	("__print_str", compile_print_str),
 	("__print_char", compile_print_char),
+	("__print_format", compile_print_format),
 ];
+
+fn compile_print_format(
+	ctx: &mut CompileCtx,
+	function_call: &ASTFunctionCall,
+) -> Result<Vec<Instruction>, String> {
+	if function_call.args.is_empty() {
+		return Err("__print_format requires at least 1 argument".into());
+	}
+
+	let format_arg = if let ASTFunctionCallArg::String(s) = &function_call.args[0] {
+		s
+	} else {
+		return Err("__print_format's first argument needs to be a static string".into());
+	};
+
+	let mut fn_instrs = Vec::new();
+	let mut format = &format_arg[..];
+	let mut arg_idx: usize = 1;
+	let mut str_buf = String::new();
+
+	while !format.is_empty() {
+		if format.starts_with("{}") {
+			// print the current string
+			let data_label = ctx.add_static_string(&str_buf);
+			let instr1 = Instruction::La {
+				dest: Register::A0,
+				label: data_label,
+			};
+			fn_instrs.push(instr1);
+			let instr2 = Instruction::Addi {
+				dst: Register::A1,
+				src: Register::Zero,
+				imm: str_buf.len() as i32,
+			};
+			fn_instrs.push(instr2);
+			fn_instrs.push(Instruction::Call("__print_str".into()));
+			str_buf = String::new();
+
+			// print the arg
+			let arg = &function_call.args[arg_idx];
+			match arg {
+				ASTFunctionCallArg::Int32(int) => {
+					let instr1 = Instruction::Addi {
+						dst: Register::A0,
+						src: Register::Zero,
+						imm: *int,
+					};
+					fn_instrs.push(instr1);
+					fn_instrs.push(Instruction::Call("__print_int".into()));
+				}
+				ASTFunctionCallArg::Char(ch) => {
+					let instr1 = Instruction::Addi {
+						dst: Register::A0,
+						src: Register::Zero,
+						imm: *ch as i32,
+					};
+					fn_instrs.push(instr1);
+					fn_instrs.push(Instruction::Call("__print_char".into()));
+				}
+				ASTFunctionCallArg::String(string) => {
+					let data_label = ctx.add_static_string(string);
+					let instr1 = Instruction::La {
+						dest: Register::A0,
+						label: data_label,
+					};
+					fn_instrs.push(instr1);
+					let instr2 = Instruction::Addi {
+						dst: Register::A1,
+						src: Register::Zero,
+						imm: string.len() as i32,
+					};
+					fn_instrs.push(instr2);
+					fn_instrs.push(Instruction::Call("__print_str".into()));
+				}
+				_ => {
+					return Err(format!(
+						"Internal function '{}' doesn't accept argument {:#?}",
+						function_call.name, arg
+					))
+				}
+			}
+			format = &format[2..];
+			arg_idx += 1;
+		} else {
+			str_buf.push_str(&format[0..1]);
+			format = &format[1..];
+		}
+	}
+
+	if !str_buf.is_empty() {
+		let data_label = ctx.add_static_string(&str_buf);
+		let instr1 = Instruction::La {
+			dest: Register::A0,
+			label: data_label,
+		};
+		fn_instrs.push(instr1);
+		let instr2 = Instruction::Addi {
+			dst: Register::A1,
+			src: Register::Zero,
+			imm: str_buf.len() as i32,
+		};
+		fn_instrs.push(instr2);
+		fn_instrs.push(Instruction::Call("__print_str".into()));
+	}
+
+	Ok(fn_instrs)
+}
 
 fn compile_print_int(
 	_ctx: &mut CompileCtx,
